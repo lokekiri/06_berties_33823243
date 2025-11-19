1e64c54f33248609b868fbe436443f0116965329
lokekiri <lokello16@gmail.com>
2025-11-19 21:31:29 +0000

    lower case

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -5,4 +5,4 @@ USE berties_books;
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
 
 INSERT INTO users (username, firstname, lastname, email, hashedPassword)
-VALUES ('gold', 'Gold', 'Smiths', 'gold@example.com', '$2b$10$EHc6m5HH.Nr7jIQ6gUR9h.DQogIFLlpbp4LV10DTSpKxl1x/JDFMW');
+VALUES ('gold', 'gold', 'smiths', 'gold@example.com', '$2b$10$EHc6m5HH.Nr7jIQ6gUR9h.DQogIFLlpbp4LV10DTSpKxl1x/JDFMW');
