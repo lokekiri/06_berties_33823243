a2e69795fd871425f7e63c49f6640943bbb756b0
lokekiri <lokello16@gmail.com>
2025-11-25 21:02:24 +0000

    fix books redirectlogin on books.js

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId) {
-        res.redirect('/users/login'); 
+        res.redirect('../users/login'); 
     } else {
         next();
     }
