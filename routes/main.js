f5c9a6873743eca8656e3b5431b9869c5892e0e8
lokekiri <lokello16@gmail.com>
2025-12-01 22:25:00 +0000

    weather berties books without api key

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const request = require('request');
 
 // Handle our routes
 router.get('/',function(req, res, next){
@@ -11,5 +12,38 @@ router.get('/about',function(req, res, next){
     res.render('about.ejs')
 });
 
-// Export the router object so index.js can access it
-module.exports = router
+router.get('/weather', function (req, res, next) {
+
+    let city = req.query.city || "London";
+    let apiKey = ''; //its on VM not github dont want it 2 b stolen
+    let url = ``; // same as above
+
+    request(url, function (err, response, body) {
+        if (err) {
+            return next(err);  
+        }
+
+        let weather;
+
+        try {
+            weather = JSON.parse(body);
+        } catch (parseError) {
+            return res.render("weather.ejs", {
+                weather: null,
+                error: "Unexpected API response."
+            });
+        }
+
+        if (!weather || !weather.main) {
+            return res.render("weather.ejs", {
+                weather: null,
+                error: "No data found. Check the city name."
+            });
+        }
+
+        res.render("weather.ejs", {
+            weather: weather,
+            error: null
+        });
+    });
+});
